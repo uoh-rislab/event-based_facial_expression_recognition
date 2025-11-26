b37e7c1c2a21cb8f870f0d2e3f7c0b7c6a0041d1
ibugueno <ignacio.bugueno@live.com>
2025-11-26 11:22:21 +0100

    memory increased

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,10 @@
 
 docker rm -fv ignacio_event_fer
 
-docker run -it --gpus '"device=0"' --name ignacio_event_fer -v /home/ignacio.bugueno/cachefs/event_fer/input:/app/input -v /home/ignacio.bugueno/cachefs/event_fer/output:/app/results ignacio_event_fer
+docker run -it \
+  --gpus '"device=0"' \
+  --name ignacio_event_fer \
+  --shm-size=8g \
+  -v /home/ignacio.bugueno/cachefs/event_fer/input:/app/input \
+  -v /home/ignacio.bugueno/cachefs/event_fer/output:/app/results \
+  ignacio_event_fer
